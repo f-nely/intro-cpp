657317aa2db45b42bc0d7185cd8332133c0faaec
nely de faria <nelly.nf54@gmail.com>
2022-03-27 14:58:34 -0300

    Criando arquivo variaveis.cpp

---
+++ intro-cpp/variaveis.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 5;
+	cout << "número inteiro: " << num << endl;
+	
+	float real = 7.8;
+	cout << "número ponto flutuante: " << real << endl;
+	
+	double real2 = 5.2e99;
+	cout << "tipo double: " << real2 << endl;
+	
+	bool booleano = false;
+	cout << "tipo bool: " << booleano << endl;
+	
+	char letra = 'b';
+	cout << "tipo char: " << letra << endl;
+	
+	string palavra = "bola";
+	cout << "tipo string: " << palavra << endl;
+	
+	return 0;
+}
